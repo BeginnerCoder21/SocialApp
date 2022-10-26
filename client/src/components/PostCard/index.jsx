5ce01ff8522f5191bc79712287d32d49c5751992
BeginnerCoder21 <anushka.war21@gmail.com>
2022-10-26 20:31:58 +0530

    new frontend

--- client/src/components/PostCard/index.jsx
+++ client/src/components/PostCard/index.jsx
@@ -1,30 +1,39 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import moment from 'moment';
+import './index.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import CardGroup from 'react-bootstrap/CardGroup';
 
-function PostCard({post: { body, createdAt, id, username}}) {
+function PostCard({ post: { body, createdAt, id, username, likeCount } }) {
     return (
-        
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="" />
-                <Card.Body>
-                    <Card.Title>{username}</Card.Title>
-                    {moment(createdAt).fromNow()}
-                    <Card.Text>
-                        {body}
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                </ListGroup>
-                <Card.Body>
-                    {/* <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link> */}
-                </Card.Body>
-            </Card>
+        <Row xs={2} md={3} className="g-4">
+            {Array.from({ length: 4 }).map((_, idx) => (
+                <Col>
+                
+                    <Card>
+                    <Card.Img variant="top" className="card-img" src="https://img.freepik.com/free-vector/flat-design-lake-scenery_23-2149161405.jpg?w=2000" />
+                        <Card.Body>
+                        
+                            <Card.Title>{username}</Card.Title>
+                            <Card.Link href={`/posts/${id}`}> {moment(createdAt).fromNow(true)}</Card.Link>
+                           
+                            <Card.Text>
+                                {body}
+                            </Card.Text>
+                            <Card.Body>
+                                <Button variant="primary">Like {likeCount}</Button>
+                                <Button variant="primary">Comment</Button>
+                            </Card.Body>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+
     );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,51 +2,50 @@ import React, { useContext } from "react";
 import { Button, Alert } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../components/NavBar";
-import axios from "axios";
 import { useQuery, gql } from "@apollo/client";
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import { FETCH_POSTS_QUERY } from '../util/graphql.js';
+import PostCard from "../components/PostCard";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import PostCard from "../components/PostCard";
+import './Home.css';
 
 function Home() {
   const { data, loading, error } = useQuery(FETCH_POSTS_QUERY);
   if (data) {
-    console.log(data.getPosts);
+    console.log(data);
   }
   // const { data, loading, error } = useQuery(FETCH_POSTS_QUERY);
   // if(data){
   //   console.log(data);
   // }
-
-
-
   // const posts = data.getPosts;
   //  console.log(posts);
   return (
     <div>
       <NavBar />
-      <h1> Recent Posts</h1>
-
-      {loading ? (
-        <h1>Loading Posts..</h1>
-      ) : (
-        data.getPosts && data.getPosts.map((post) => {
-          <ul >
-            <li key={post.id}>
+      <h1> Recent Posts </h1>
+      <Container>
+        
+          {loading ? (
+            <h1> Loading Posts.. </h1>
+          ) : (
+            data.getPosts.map((post) => (
+              <Row key={post.id} className={post.id}>
               <PostCard post={post} />
-            </li>
-          </ul>
-        })
-      )}
-
-
-
+              </Row>
+            ))
+          )}
+        
+      </Container>
     </div>
   );
 }
 
 export default Home;
+
+{/* <ul>
+            <li key={post.id}>
+            <PostCard post={post} key={post.id} />
+            </li>
+        </ul> */}
